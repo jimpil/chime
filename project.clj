709e2f810b9519ba080fe05b2f532d51ed0a5214
Dimitrios Piliouras <jimpil1985@gmail.com>
2023-02-11 15:07:29 +0000

    Version 1.0.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jimpil/chime "1.0.11-SNAPSHOT"
+(defproject jimpil/chime "1.0.11"
   :description "Flexible scheduling primitives for Clojure"
 
   :url "https://github.com/jimpil/chime"
